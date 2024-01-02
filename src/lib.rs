d35c464e535b2afbac2ff06a4f63ed2c72cd138d
Vixen <infvix@gmail.com>
2024-01-02 22:02:58 +0100

    Fix compiler errors

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,13 @@ pub mod prelude;
 pub mod prototype_material;
 
 use bevy::prelude::*;
-use bevy_embedded_assets::EmbeddedAssetPlugin;
 use rust_embed::RustEmbed;
 
 pub struct DevPlugins;
 
 impl Plugin for DevPlugins {
     fn build(&self, app: &mut App) {
-        app.add_plugins((
-            EmbeddedAssetPlugin::default(),
-            prototype_material::PrototypeMaterialPlugin,
-        ));
+        app.add_plugins(prototype_material::PrototypeMaterialPlugin);
     }
 }
 
